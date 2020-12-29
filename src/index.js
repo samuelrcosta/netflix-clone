41057dfc0860c9f59c574cc3747b1abe5a5902b6
Samuel Costa <samu.rcosta@gmail.com>
2020-12-29 17:01:47 -0300

    Added firebase connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
+import FirebaseContext from './contexts/firebase';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
+import { firebase } from './lib/firebase.prod';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,19 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+// import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const firebase = Firebase.initializeApp(config);
+
+// seedDatabase(firebase);
+
+export { firebase };
