c1b0ad2185acf1cc836b4589d7031472bca97316
Samuel Costa <samu.rcosta@gmail.com>
2020-12-29 00:37:23 -0300

    Added accordion and opt-form

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import JumbotronContainer from './containers/jumbotron';
 import FooterContainer from './containers/footer';
+import FaqContainer from './containers/faqs';
 
 export function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqContainer />
       <FooterContainer />
     </>
   );
