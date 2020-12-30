0655a3fbe08b38c32163713d1987b7610b45c228
Samuel Costa <samu.rcosta@gmail.com>
2020-12-29 22:20:52 -0300

    Public and private routes control, authState hook

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 
 export function App() {
+  const { user } = useAuthListener();
+
   return (
     <BrowserRouter>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <SignIn />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <SignUp />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <Switch>
+        {/* Not logged routes */}
+        <IsUserRedirect exact path={ROUTES.SIGN_IN} user={user} loggedInPath={ROUTES.BROWSE}>
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect exact path={ROUTES.SIGN_UP} user={user} loggedInPath={ROUTES.BROWSE}>
+          <SignUp />
+        </IsUserRedirect>
+        <IsUserRedirect exact path={ROUTES.HOME} user={user} loggedInPath={ROUTES.BROWSE}>
+          <Home />
+        </IsUserRedirect>
+
+        <ProtectedRoute exact path={ROUTES.BROWSE} user={user}>
+          <Browse />
+        </ProtectedRoute>
+      </Switch>
     </BrowserRouter>
   );
 }
