60c24b7ef8f99ac50a1c152757fb6157afc0742c
Samuel Costa <samu.rcosta@gmail.com>
2021-01-04 22:45:35 -0300

    Added search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import SelectProfileContainer from './profiles';
 import FirebaseContext from '../contexts/firebase';
 import * as ROUTES from '../constants/routes';
@@ -26,6 +27,17 @@ export default function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
